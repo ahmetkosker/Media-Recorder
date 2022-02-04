2452a16294b01586140d9ff8b8be8899becd76f6
root <root@DESKTOP-B9MR9S8.localdomain>
2022-02-04 16:54:56 +0300

    first commit,

---
+++ media.js
@@ -0,0 +1,44 @@
+const express = require('express')
+var multer = require('multer');
+var bodyParser = require('body-parser');
+const child_process = require('child_process');
+
+var fs = require('fs');
+
+const app = express()
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var upload = multer({ dest: __dirname + "../../../dejavu/mp3/" });
+var type = upload.single('upl');
+
+app.get('/', async function (req, res) {
+  res.sendFile(__dirname + '/front.html');
+})
+
+app.get('/test', function (req, res) {
+
+  res.send('test')
+
+});
+
+app.post('/test', type, function (req, res) {
+
+  fs.rename(req.file.path, '/root/dejavu/mp3/' + req.file.originalname, function (err) {
+    if (err) console.log('ERROR: ' + err);
+  });
+
+  const process = child_process.exec('docker exec 5ee787688520 python example_docker_postgres.py mp3/' + req.file.originalname, function (error, stdout, stderr) {
+
+    if (error) throw error;
+
+    res.json(stdout)
+
+  });
+
+});
+
+
+app.listen(3000)
